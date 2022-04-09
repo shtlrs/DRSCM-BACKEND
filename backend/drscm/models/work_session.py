b2ffd322df2f0a7a467d59bf6983f928e7b9358d
Amrou Bellalouna <Amrou.bellalouna@expensya.com>
2022-04-09 14:17:16 +0100

    create worksession timestamps out of the date utils

--- backend/drscm/models/work_session.py
+++ backend/drscm/models/work_session.py
@@ -1,5 +1,6 @@
-from django.db import models
 import uuid
+from django.db import models
+from backend.utils.date import get_timestamp_with_null_seconds
 
 
 class WorkSession(models.Model):
@@ -7,3 +8,15 @@ class WorkSession(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     start_timestamp = models.IntegerField(editable=True)
     end_timestamp = models.IntegerField(editable=True)
+
+
+    @classmethod
+    def create(cls):
+        work_session = cls(start_timestamp=get_timestamp_with_null_seconds())
+        return work_session
+
+    def save(
+        self, force_insert=False, force_update=False, using=None, update_fields=None
+    ):
+        self.end_timestamp = get_timestamp_with_null_seconds()
+        super(WorkSession, self).save(force_insert=force_insert, force_update=force_update, using=using, update_fields=update_fields)

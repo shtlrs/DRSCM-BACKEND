005e8902a723057db4d394cfc483b3358c4014bc
Amrou Bellalouna <Amrou.bellalouna@expensya.com>
2022-04-10 11:48:58 +0100

    replace client_id with pk

--- backend/drscm/urls.py
+++ backend/drscm/urls.py
@@ -3,5 +3,5 @@ from drscm.views import ClientsList, ClientDetails
 
 urlpatterns = [
     path('clients/', ClientsList.as_view()),
-    path('clients/<uuid:client_id>', ClientDetails.as_view())
+    path('clients/<uuid:pk>', ClientDetails.as_view())
 ]

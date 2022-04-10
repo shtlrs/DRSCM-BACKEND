e793d8cfe71361dfbcae5bd1c2e6be82fa37b252
Karem Sfar Gandoura <Karem.sfargandoura@expensya.com>
2022-04-10 12:14:46 +0100

    add navbar and tailwind css stuff

--- front/src/app/app.module.ts
+++ front/src/app/app.module.ts
@@ -4,6 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AuthModule } from './auth/auth.module';
+import { NavbarModule } from './navbar/navbar.module';
 
 @NgModule({
   declarations: [
@@ -12,7 +13,8 @@ import { AuthModule } from './auth/auth.module';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    AuthModule
+    AuthModule,
+    NavbarModule
   ],
   providers: [],
   bootstrap: [AppComponent]

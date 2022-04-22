fc79c2d5f6ab3744f35ed8a213656ec2562a66d6
Amrou Bellalouna <48383734+shtlrs@users.noreply.github.com>
2022-04-22 20:07:40 +0100

    subclass BaseUSer and add UserManager

---
+++ backend/drscm/models/user.py
@@ -0,0 +1,47 @@
+import uuid
+
+from django.db import models
+from django.contrib.auth.models import PermissionsMixin
+from django.contrib.auth.base_user import AbstractBaseUser
+from django.utils import timezone
+
+from drscm.managers.user import UserManager
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+
+    ADMIN = 1
+    MANAGER = 2
+    EMPLOYEE = 3
+
+    ROLES = (
+        (ADMIN, 'Admin'),
+        (MANAGER, 'Manager'),
+        (EMPLOYEE, 'Employee'))
+
+
+    id = models.UUIDField(primary_key=True, unique=True, editable=False,
+                          default=uuid.uuid4, verbose_name='Public identifier')
+    email = models.EmailField(unique=True)
+    first_name = models.CharField(max_length=40, blank=False)
+    last_name = models.CharField(max_length=40, blank=False)
+    role = models.PositiveSmallIntegerField(choices=ROLES, blank=True, null=True, default=3)
+    date_joined = models.DateTimeField(auto_now_add=True)
+    is_active = models.BooleanField(default=True)
+    is_deleted = models.BooleanField(default=False)
+    created_date = models.DateTimeField(default=timezone.now)
+    modified_date = models.DateTimeField(default=timezone.now)
+    created_by = models.EmailField()
+    modified_by = models.EmailField()
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+
+    objects = UserManager()
+
+    def __str__(self):
+        return self.email
+
+    class Meta:
+        verbose_name = 'user'
+        verbose_name_plural = 'users'

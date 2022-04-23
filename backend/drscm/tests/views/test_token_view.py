9f6f9a265d24d78669923b5bf9a004736cde9b7e
Amrou Bellalouna <48383734+shtlrs@users.noreply.github.com>
2022-04-23 22:01:40 +0100

    add token view tests except for test_refresh_token_with_expired_refresh_token

--- backend/drscm/tests/views/test_token_view.py
+++ backend/drscm/tests/views/test_token_view.py
@@ -1,42 +1,65 @@
 from django.urls import reverse
 from rest_framework import status
 from rest_framework.test import APITestCase
-from rest_framework_simplejwt.views import TokenObtainPairView
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 from drscm.tests.helpers.user import create_random_user
 from rest_framework_simplejwt.serializers import TokenRefreshSerializer
 
 
 class TokenViewTest(APITestCase):
 
-    def test_request_token_with_valid_credentials(self):
-
+    @classmethod
+    def setUpTestData(cls):
         user = create_random_user()
-        user.save()
-
-        data = {
+        cls.data = {
             'username': user.username,
             'email': user.email,
             'password': user.raw_password
         }
+        cls.user = user
 
-        url = reverse(TokenObtainPairView.__name__)
-        response = self.client.post(url, data=data)
+    def test_request_token_with_valid_credentials(self):
+
+        self.user.save()
+        create_token_url = reverse(TokenObtainPairView.__name__)
+        response = self.client.post(create_token_url, data=self.data)
 
         self.assertEqual(status.HTTP_200_OK, response.status_code)
         serializer = TokenRefreshSerializer(data=response.json())
         self.assertEqual(True, serializer.is_valid())
 
 
-
-
     def test_request_token_with_invalid_credentials(self):
-        self.fail()
+        self.user.save()
+        self.data['username'] = "invalid_username"
+
+        create_token_url = reverse(TokenObtainPairView.__name__)
+        response = self.client.post(create_token_url, data=self.data)
+
+        self.assertEqual(status.HTTP_401_UNAUTHORIZED, response.status_code)
 
     def test_request_token_with_non_existing_user(self):
-        self.fail()
+        self.data['username'] = "non_existing_username"
+
+        create_token_url = reverse(TokenObtainPairView.__name__)
+        response = self.client.post(create_token_url, data=self.data)
+
+        self.assertEqual(status.HTTP_401_UNAUTHORIZED, response.status_code)
 
     def test_refresh_token(self):
-        self.fail()
+        self.user.save()
+        create_token_url = reverse(TokenObtainPairView.__name__)
+        response = self.client.post(create_token_url, data=self.data)
+        self.assertEqual(status.HTTP_200_OK, response.status_code)
+
+        refresh_token = response.json().get('refresh')
+        refresh_token_url = reverse(TokenRefreshView.__name__)
+        data = {
+            'refresh': refresh_token
+        }
+
+        response = self.client.post(refresh_token_url, data=data)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
 
     def test_refresh_token_with_expired_refresh_token(self):
         self.fail()

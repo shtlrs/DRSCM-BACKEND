c37c262ec449ccda8ece124a89563b92e3fa6928
Amrou Bellalouna <48383734+shtlrs@users.noreply.github.com>
2022-04-23 14:25:35 +0100

    update get_query_set of the user's view

--- backend/drscm/views/user.py
+++ backend/drscm/views/user.py
@@ -12,3 +12,11 @@ class ListUsersView(generics.ListAPIView):
     serializer_class = UserSerializer
     permission_classes = (IsAuthenticated,)
 
+    def get_queryset(self):
+        user = self.request.user
+        if user.is_superuser:
+            query_set = User.objects.all()
+        else:
+            query_set = User.objects.filter(pk=user.id)
+
+        return query_set

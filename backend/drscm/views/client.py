8c854e739b464e09e3e62f43ae31adda1896d94d
Amrou Bellalouna <48383734+shtlrs@users.noreply.github.com>
2022-04-24 23:26:37 +0100

    Update documentation descriptions & operation ids

--- backend/drscm/views/client.py
+++ backend/drscm/views/client.py
@@ -2,10 +2,14 @@ from rest_framework.permissions import IsAuthenticated
 from drscm.models import Client
 from drscm.permissions.model.is_owner import IsSuperUserOrOwner
 from drscm.serializers.client import ClientSerializer
-from drf_spectacular.utils import extend_schema
+from drf_spectacular.utils import extend_schema, extend_schema_view
 from rest_framework import generics
 
 
+@extend_schema_view(
+     get=extend_schema(description='Returns the list of the clients available for the user', operation_id="List clients"),
+     post=extend_schema(description='Creates a new client for the user', operation_id="Create client"),
+ )
 class CreateAndListClientsView(generics.ListCreateAPIView):
 
     view_name = "create_or_list_clients_view"
@@ -26,6 +30,13 @@ class CreateAndListClientsView(generics.ListCreateAPIView):
         return queryset
 
 
+@extend_schema_view(
+    get=extend_schema(description='Returns the details of a particular client', operation_id="Get client"),
+    delete=extend_schema(description='Deletes a particular client', operation_id="Delete client"),
+    put=extend_schema(description='Updates a client fully', operation_id="Update client"),
+    patch=extend_schema(description='Patches a client by doing a partial update of specific fields',
+                        operation_id="Patch client")
+ )
 class ClientDetailsView(generics.RetrieveUpdateDestroyAPIView):
 
     view_name = "client_details_view"
--- backend/drscm/views/user.py
+++ backend/drscm/views/user.py
@@ -1,9 +1,14 @@
+from drf_spectacular.utils import extend_schema_view, extend_schema
 from rest_framework.permissions import IsAuthenticated
 from drscm.serializers.user import UserSerializer
 from drscm.models.user import User
 from rest_framework import generics
 
 
+@extend_schema_view(
+     get=extend_schema(description='Returns the list of the available users',
+                       operation_id="List users"),
+ )
 class ListUsersView(generics.ListAPIView):
 
     view_name = "list_users_view"

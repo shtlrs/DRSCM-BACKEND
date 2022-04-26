9f95473be29949b3500f39df9075ff7d8717ae5c
Amrou Bellalouna <48383734+shtlrs@users.noreply.github.com>
2022-04-26 14:19:37 +0100

    remove endpoint to black list token

--- backend/drscm/tests/views/test_token_view.py
+++ backend/drscm/tests/views/test_token_view.py
@@ -65,7 +65,3 @@ class TokenViewTest(APITestCase):
 
         response = self.client.post(refresh_token_url, data=data)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
-
-
-    def test_black_list_token(self):
-        self.fail()

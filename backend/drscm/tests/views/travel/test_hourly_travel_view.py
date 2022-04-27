ca2fbcaf657b9389f7e302bbb0c1b2e5497f3d6c
Amrou Bellalouna <48383734+shtlrs@users.noreply.github.com>
2022-04-27 18:08:09 +0100

    implement all tests for the hourly travel view

--- backend/drscm/tests/views/travel/test_hourly_travel_view.py
+++ backend/drscm/tests/views/travel/test_hourly_travel_view.py
@@ -1,12 +1,17 @@
+from uuid import UUID
+from django.urls import reverse
+from rest_framework import status
 from rest_framework.test import APITestCase
 from rest_framework_simplejwt.tokens import RefreshToken
-
+from drscm.models import HourlyTravel
+from drscm.serializers import HourlyTravelSerializer
 from drscm.tests.helpers import (
     create_random_user,
     create_random_client,
     create_random_project,
     create_random_hourly_travel,
 )
+from drscm.views import CreateAndListHourlyTravelsView, HourlyTravelDetailsView
 
 
 class HourlyTravelViewTests(APITestCase):
@@ -28,26 +33,83 @@ class HourlyTravelViewTests(APITestCase):
         )
         cls.user_token = RefreshToken.for_user(cls.user)
 
+    def setUp(self) -> None:
+        self.client.credentials(HTTP_AUTHORIZATION=f"JWT {self.super_token.access_token}")
+
     def test_add_new_hourly_travel_record(self):
-        self.fail()
+        url = reverse(CreateAndListHourlyTravelsView.view_name)
+        data = HourlyTravelSerializer(instance=self.super_hourly_travel).data
+        response = self.client.post(path=url, data=data)
+        self.assertEqual(status.HTTP_201_CREATED, response.status_code)
 
     def test_add_new_hourly_travel_record_with_non_existent_project(self):
-        self.fail()
-
-    def test_update_travel_record(self):
-        self.fail()
+        url = reverse(CreateAndListHourlyTravelsView.view_name)
+        project = create_random_project(client=self.super_client)
+        hourly_travel_record = create_random_hourly_travel(project=project)
+        data = HourlyTravelSerializer(instance=hourly_travel_record).data
+        response = self.client.post(path=url, data=data)
+        self.assertEqual(status.HTTP_400_BAD_REQUEST, response.status_code)
+        self.assertEqual(
+            {"project": [f'Invalid pk "{project.id}" - object does not exist.']},
+            response.json(),
+        )
 
     def test_patch_hourly_travel_record(self):
-        self.fail()
+        self.super_hourly_travel.save()
+        url = reverse(HourlyTravelDetailsView.view_name, args=[self.super_hourly_travel.id])
+        new_timestamp = self.super_hourly_travel.timestamp + 200
+        self.super_hourly_travel.timestamp = new_timestamp
+        data = HourlyTravelSerializer(instance=self.super_hourly_travel).data
+        response = self.client.patch(path=url, data=data)
+        self.assertEqual(status.HTTP_200_OK, response.status_code)
+        hourly_travel_record = HourlyTravel.objects.get(pk=self.super_hourly_travel.id)
+        self.assertEqual(hourly_travel_record.timestamp, self.super_hourly_travel.timestamp)
 
     def test_delete_hourly_travel_record(self):
-        self.fail()
+        self.super_hourly_travel.save()
+        url = reverse(HourlyTravelDetailsView.view_name, args=[self.super_hourly_travel.id])
+        data = HourlyTravelSerializer(instance=self.super_hourly_travel).data
+        response = self.client.delete(path=url, data=data)
+        self.assertEqual(status.HTTP_204_NO_CONTENT, response.status_code)
+        hourly_travel_records = HourlyTravel.objects.filter(owner=self.superuser)
+        self.assertEqual(len(hourly_travel_records), 0)
 
     def test_list_hourly_travel_records_per_appropriate_user(self):
-        self.fail()
+        self.super_hourly_travel.save()
+        self.user_hourly_travel.save()
+        url = reverse(CreateAndListHourlyTravelsView.view_name)
+        response = self.client.get(path=url)
+        hourly_travel_ids = [
+            UUID(hourly_travel.get("id")) for hourly_travel in response.json()
+        ]
+
+        self.assertEqual(status.HTTP_200_OK, response.status_code)
+        self.assertEqual(len(hourly_travel_ids), 2)
+        self.assertIn(self.super_hourly_travel.id, hourly_travel_ids)
+        self.assertIn(self.super_hourly_travel.id, hourly_travel_ids)
+
+        self.client.credentials(HTTP_AUTHORIZATION=f"JWT {self.user_token.access_token}")
+        response = self.client.get(path=url)
+        hourly_travel_ids = [
+            UUID(hourly_travel.get("id")) for hourly_travel in response.json()
+        ]
+
+        self.assertEqual(status.HTTP_200_OK, response.status_code)
+        self.assertEqual(len(hourly_travel_ids), 1)
+        self.assertNotIn(self.super_hourly_travel.id, hourly_travel_ids)
+        self.assertIn(self.user_hourly_travel.id, hourly_travel_ids)
 
     def test_retrieve_hourly_travel_record(self):
-        self.fail()
+        self.super_hourly_travel.save()
+
+        url = reverse(HourlyTravelDetailsView.view_name, args=[self.super_hourly_travel.id])
+        response = self.client.get(url)
+        hourly_travel = response.json()
+
+        self.assertEqual(status.HTTP_200_OK, response.status_code)
+        self.assertEqual(UUID(hourly_travel.get("id")), self.super_hourly_travel.id)
 
     def test_retrieve_non_existent_hourly_travel_record(self):
-        self.fail()
+        url = reverse(HourlyTravelDetailsView.view_name, args=[self.super_hourly_travel.id])
+        response = self.client.get(url)
+        self.assertEqual(status.HTTP_404_NOT_FOUND, response.status_code)

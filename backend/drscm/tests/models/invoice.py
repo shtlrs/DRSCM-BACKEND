031a12df6b7607167ad1fae5cf335ac001efe9df
Amrou Bellalouna <48383734+shtlrs@users.noreply.github.com>
2022-04-27 23:27:20 +0100

    add empty inovice model tests

---
+++ backend/drscm/tests/models/invoice.py
@@ -0,0 +1,43 @@
+from rest_framework.test import APITestCase
+
+
+
+class InvoiceModelTests(APITestCase):
+
+
+    def test_create_new_invoice(self):
+        self.fail()
+
+    def test_create_new_invoice_with_no_project(self):
+        self.fail()
+
+    def test_create_new_invoice_with_no_work_sessions(self):
+        self.fail()
+
+    def test_create_new_invoice_with_no_fixed_travels(self):
+        self.fail()
+
+    def test_create_new_invoice_with_no_hourly_travels(self):
+        self.fail()
+
+    def test_patch_invoice_work_sessions(self):
+        self.fail()
+
+    def test_patch_invoice_hourly_travel_sessions(self):
+        self.fail()
+
+    def test_patch_invoice_fixed_travel_sessions(self):
+        self.fail()
+
+    def test_delete_invoice(self):
+        self.fail()
+
+    def test_delete_invoice_work_sessions(self):
+        self.fail()
+
+    def test_delete_invoice_fixed_travel_sessions(self):
+        self.fail()
+
+    def test_delete_invoice_hourly_travel_sessions(self):
+        self.fail()
+

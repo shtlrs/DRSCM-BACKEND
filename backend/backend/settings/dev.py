592f1db3293bcb098e1c5b810adfbd9774585ca9
Amrou Bellalouna <48383734+shtlrs@users.noreply.github.com>
2022-04-30 12:41:55 +0100

    enable logging

--- backend/backend/settings/dev.py
+++ backend/backend/settings/dev.py
@@ -8,3 +8,21 @@ DATABASES = {
         "NAME": BASE_DIR / "test-db.sqlite3",
     }
 }
+
+
+LOGGING = {
+    'version': 1,
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'loggers': {
+        'django.db.backends': {
+            'level': 'DEBUG',
+        },
+    },
+    'root': {
+        'handlers': ['console'],
+    }
+}

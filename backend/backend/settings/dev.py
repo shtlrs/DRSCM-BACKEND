e2282e9df8df6953c34b8e9d33d5bce8667b9b51
Amrou Bellalouna <48383734+shtlrs@users.noreply.github.com>
2022-04-30 21:47:55 +0100

    Revert "delete old config files"
    This reverts commit 392873f99a030ed10b2e6809ef011cb4d9739e33.

---
+++ backend/backend/settings/dev.py
@@ -0,0 +1,28 @@
+from .base import *
+
+DEBUG = True
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "test-db.sqlite3",
+    }
+}
+
+
+# LOGGING = {
+#     "version": 1,
+#     "handlers": {
+#         "console": {
+#             "class": "logging.StreamHandler",
+#         },
+#     },
+#     "loggers": {
+#         "django.db.backends": {
+#             "level": "DEBUG",
+#         },
+#     },
+#     "root": {
+#         "handlers": ["console"],
+#     },
+# }

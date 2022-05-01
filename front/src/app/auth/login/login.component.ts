063e68674354032ab105101065553a3c110e7a1e
Karem Sfar Gandoura <Karem.sfargandoura@expensya.com>
2022-05-01 14:47:31 +0200

    import Reactive Forms Module Stuff to Login Module

--- front/src/app/auth/login/login.component.ts
+++ front/src/app/auth/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +7,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+  loginForm?: FormGroup;
 
-  constructor() { }
+  constructor(
+    private formBuilder: FormBuilder,
+  ) {
+    // also navigate to dashboard if already logged in
+  }
 
   ngOnInit(): void {
+    this.loginForm = this.formBuilder.group({
+      username: ['', Validators.required],
+      password: ['', Validators.required]
+    });
   }
 
 }

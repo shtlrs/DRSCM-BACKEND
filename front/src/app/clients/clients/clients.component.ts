b9f1d82c08103b7b136b31797ecc9c565d60e9f5
Amrou Bellalouna <48383734+shtlrs@users.noreply.github.com>
2022-05-14 16:55:16 +0100

    add filtering on clients

--- front/src/app/clients/clients/clients.component.ts
+++ front/src/app/clients/clients/clients.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ClientsService } from "@clients/services/clients.service";
 import {Client} from "@clients/models/client";
+import {tap} from "rxjs";
 
 @Component({
   selector: 'app-clients',
@@ -10,12 +11,18 @@ import {Client} from "@clients/models/client";
 export class ClientsComponent implements OnInit {
 
   clients: Array<Client> = [];
+  clientsToDisplay: Array<Client> = [];
   constructor(private clientService: ClientsService) { }
 
   ngOnInit(): void {
     this.clientService.getClients().subscribe(
-      clients => this.clients = clients
+      clients => (this.clients = clients) && (this.clientsToDisplay = clients)
     );
+  }
+
+  filter(event: any){
+    const val = event.target.value.toLowerCase();
+    this.clientsToDisplay = this.clients.filter(client => client.name.toLowerCase().includes(val));
   }
 
 }

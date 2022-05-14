93ccfc888be9b34502eb04d4d3cce08cc96f8c0f
Amrou Bellalouna <48383734+shtlrs@users.noreply.github.com>
2022-05-14 14:08:05 +0100

    update apiUrl with an ending slash

--- front/src/environments/environment.ts
+++ front/src/environments/environment.ts
@@ -3,7 +3,7 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  apiUrl: 'https://localhost',
+  apiUrl: 'https://localhost/',
   production: false
 };
 

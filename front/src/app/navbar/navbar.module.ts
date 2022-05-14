68811a9419336bd1e1a3005a6b93778d1de17beb
Amrou Bellalouna <48383734+shtlrs@users.noreply.github.com>
2022-05-14 16:54:56 +0100

    add RouterModule to NavBArModule

--- front/src/app/navbar/navbar.module.ts
+++ front/src/app/navbar/navbar.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NavbarComponent } from './navbar/navbar.component';
-
+import {RouterModule} from "@angular/router";
 
 
 @NgModule({
@@ -9,7 +9,8 @@ import { NavbarComponent } from './navbar/navbar.component';
     NavbarComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule,
   ],
   exports: [NavbarComponent],
 })
